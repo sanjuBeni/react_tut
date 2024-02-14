76ca4c328aee9593f15fac6917fbbd700ccc79a3
sanjauBeni <sanjaysewa999@gmail.com>
2024-02-14 23:34:37 +0530

    useMemo hook use for stop re-rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 import './Style.css';
 import style from './style.module.css';
@@ -10,53 +10,29 @@ function App() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState(100)
 
-  const handleForm  = (e) =>{
-    e.preventDefault();
-    console.log(formData);
-  }
 
-  const handleCheckbox = (e) => {
-    setFormData({...formData, [e.target.name] : e.target.checked})
-  }
-
-  const handleData  =  (e) => {
-    setFormData({...formData, [e.target.name] : e.target.value})
-  }
-  const hello = () =>  "hello";
-  
-  useEffect(()=> {
-      console.log("run useEffet hook and count also run", count);  
-  }, [count]);
-
-  useEffect(()=> {
-      console.log("run useEffet hook and data also run", data);  
-  }, [data]);
+  const memoCall = useMemo(() => {
+        console.log("count call");
+        return count;
+    },[count]);
 
   
-  
+    const countFun = () => {
+        console.log("count call");
+        return count;
+    }
 
   return (
     <div className="App">
-      <form onSubmit={handleForm}> 
-        <input type='text' name='name' onChange={handleData} placeholder='Enter name'/>
-        <br></br><br/>
-        <select name='select' onChange={handleData}>
-            <option>Select Value</option>
-            <option>React</option>
-            <option>Laravel</option>
-        </select>
-        <br></br><br/>
-        <input type='checkbox' name = "tnc" onChange={handleCheckbox} />Accept Term & Condition
-        <br></br><br/>
-        <button type='sumit'>Submit</button>
-      </form>
-      <h2 className={style.myclass}>{count}</h2>
+      <h2>{memoCall}</h2>
       <button onClick={() => setCount(count+1)}>Count</button>
       <h2 className='color'>{data}</h2>
       <button onClick={() => setData(data+1)}>Data</button>
-      {/* <ClassComp/> */}
     </div>
   );
+
+  
+  
 }
 
 export default App;
