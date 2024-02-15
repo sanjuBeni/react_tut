015ab5a3c0ca44417ec53c97a3aef9910d910887
sanjauBeni <sanjaysewa999@gmail.com>
2024-02-15 23:34:09 +0530

    ref and useRef or forwordRef

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import './Style.css';
 import style from './style.module.css';
+import ForwordRefCom from './ForwordRefCom';
 // import ClassComp from './ClassComp';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState(100)
 
+  let modalOpen = useRef(null);
 
   const memoCall = useMemo(() => {
         console.log("count call");
@@ -22,12 +24,18 @@ function App() {
         return count;
     }
 
+    const myModal = () => {
+        modalOpen.current.value = 1234;
+    }
+
   return (
     <div className="App">
       <h2>{memoCall}</h2>
       <button onClick={() => setCount(count+1)}>Count</button>
       <h2 className='color'>{data}</h2>
-      <button onClick={() => setData(data+1)}>Data</button>
+      <button onClick={() => setData(data+1)}>Data</button><br/><br/>
+      <ForwordRefCom ref={modalOpen}/>
+      <button onClick={myModal}>Open Modal</button>
     </div>
   );
 
